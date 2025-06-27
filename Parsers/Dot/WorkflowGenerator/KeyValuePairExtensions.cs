b6b59929434b833cb373f13a3eea85600e9bb8a5
VladD2 <vc@rsdn.ru>
2025-06-27 03:28:47 +0300

    [DOT][Workflow] Added generating Replay enum and HandleReply method.

---
+++ Parsers/Dot/WorkflowGenerator/KeyValuePairExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkflowGenerator;
+
+public static class KeyValuePairExtensions
+{
+    public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> keyValuePair, out TKey key, out TValue value)
+    {
+        key = keyValuePair.Key;
+        value = keyValuePair.Value;
+    }
+}
